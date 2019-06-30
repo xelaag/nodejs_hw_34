a5f842bbe1420772f1643ee8772659ecc2a7fa30
Bakelev Alexander <bakelev@gmail.com>
2019-06-30 16:16:33 +0300

    add lowDb

--- controlers/products.js
+++ controlers/products.js
@@ -1,15 +1,19 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
+const dbPath = path.join(__dirname, '../models/db.json');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
-const productsPath = path.join(__dirname, '../temp/products.json');
+const adapter = new FileSync(dbPath);
+const db = low(adapter);
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ products: [], skills: [] }).write();
 
 exports.get = () =>
   new Promise(async (resolve, reject) => {
     try {
-      let products = [];
-      if (fs.existsSync(productsPath)) {
-        products = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
-      }
+      const products = db.get('products').value();
       resolve(products);
     } catch (error) {
       reject({
@@ -46,26 +50,17 @@ exports.add = ({ photo, name, price }) =>
 
       fs.renameSync(tempPath, path.join(uploadDir, photoName));
 
-      let products = [];
-      if (fs.existsSync(productsPath)) {
-        // add check that exist file
-        products = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
-      }
-
-      let newProducts = products.slice();
-      newProducts.push({
-        src: './assets/img/products/' + photoName,
-        name: name,
-        price: price
-      });
-
-      fs.writeFileSync(
-        path.join(process.cwd(), '/temp/products.json'),
-        JSON.stringify(newProducts)
-      );
+      // Add a products
+      db.get('products')
+        .push({
+          src: './assets/img/products/' + photoName,
+          name: name,
+          price: price
+        })
+        .write();
+      console.log('New Products: ' + name + ' has been saved');
       resolve(true);
     } catch (error) {
-      // eslint-disable-next-line prefer-promise-reject-errors
       reject({
         success: false,
         status: 500
--- controlers/skills.js
+++ controlers/skills.js
@@ -1,27 +1,18 @@
 /* eslint-disable no-console */
-const fs = require('fs');
 const path = require('path');
 
-const skillsPath = path.join(__dirname, '../temp/skills.json');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync(path.join(__dirname, '../models/db.json'));
+const db = low(adapter);
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ products: [], skills: [] }).write();
 
 exports.add = ({ age, concerts, cities, years }) =>
   new Promise(async (resolve, reject) => {
     try {
-      if (!age || !concerts || !cities || !years) {
-        reject('All fields are required');
-        return;
-      }
-
-      let skills = [];
-
-      try {
-        fs.accessSync(skillsPath);
-        skills = JSON.parse(fs.readFileSync(skillsPath, 'utf-8'));
-      } catch (err) {
-        console.log('No dir: ', skillsPath);
-      }
-
-      skills = [
+      // Add a skills
+      db.set('skills', [
         {
           number: age,
           text: 'Возраст начала занятий на скрипке'
@@ -38,12 +29,9 @@ exports.add = ({ age, concerts, cities, years }) =>
           number: years,
           text: 'Лет на сцене в качестве скрипача'
         }
-      ];
+      ]).write();
+      console.log('Skills has been updated');
 
-      fs.writeFileSync(
-        path.join(process.cwd(), '/temp/skills.json'),
-        JSON.stringify(skills)
-      );
       resolve(true);
     } catch (error) {
       console.log('Error to save skills: ', error);
@@ -57,10 +45,7 @@ exports.add = ({ age, concerts, cities, years }) =>
 exports.get = () =>
   new Promise(async (resolve, reject) => {
     try {
-      let skills = [];
-      if (fs.existsSync(skillsPath)) {
-        skills = JSON.parse(fs.readFileSync(skillsPath, 'utf-8'));
-      }
+      const skills = db.get('skills').value();
       resolve(skills);
     } catch (error) {
       console.log('Error to get skills: ', error);
